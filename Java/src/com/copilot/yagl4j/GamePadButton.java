563879b1ee22c2d93d94b8a5cf97bfa0366255fb
Ryan Meulenkamp <Ryan.Meulenkamp@Nedap.com>
2017-10-03 21:31:25 +0200

    :art: Update code to google format
    Resolves: #1

--- Java/src/com/copilot/yagl4j/GamePadButton.java
+++ Java/src/com/copilot/yagl4j/GamePadButton.java
@@ -6,58 +6,58 @@ package com.copilot.yagl4j;
  * @author Mark Lefering
  */
 public enum GamePadButton {
-    INVALID(-1),
-    A(0),
-    B(1),
-    X(2),
-    Y(3),
-    BACK(4),
-    GUIDE(5),
-    START(6),
-    LEFT_STICK(7),
-    RIGHT_STICK(8),
-    LEFT_SHOULDER(9),
-    RIGHT_SHOULDER(10),
-    DPAD_UP(11),
-    DPAD_DOWN(12),
-    DPAD_LEFT(13),
-    DPAD_RIGHT(14);
+  INVALID(-1),
+  A(0),
+  B(1),
+  X(2),
+  Y(3),
+  BACK(4),
+  GUIDE(5),
+  START(6),
+  LEFT_STICK(7),
+  RIGHT_STICK(8),
+  LEFT_SHOULDER(9),
+  RIGHT_SHOULDER(10),
+  DPAD_UP(11),
+  DPAD_DOWN(12),
+  DPAD_LEFT(13),
+  DPAD_RIGHT(14);
 
-    private final int value;
+  private final int value;
 
-    /**
-     * Controller button enum constructor.
-     *
-     * @param value The value of the controller button
-     */
-    GamePadButton(int value) {
-        // Setting value
-        this.value = value;
+  /**
+   * Controller button enum constructor.
+   *
+   * @param value The value of the controller button
+   */
+  GamePadButton(int value) {
+    // Setting value
+    this.value = value;
+  }
+
+  /**
+   * Gets the enum from a integer value.
+   *
+   * @param value The value to get the enum for.
+   * @return The enum.
+   * @throws IllegalArgumentException -1 <= value <= 14
+   */
+  public static GamePadButton fromValue(int value) {
+    if (value >= -1 && value <= 14) {
+      return values()[value + 1];
+    } else {
+      throw new IllegalArgumentException(
+          String.format("Controller value %d invalid", value)
+      );
     }
+  }
 
-    /**
-     * Gets the enum from a integer value.
-     *
-     * @param value The value to get the enum for.
-     * @return The enum.
-     * @throws IllegalArgumentException -1 <= value <= 14
-     */
-    public static GamePadButton fromValue(int value) throws IllegalArgumentException {
-        // Checks if the value is within range
-        if (value >= -1 && value <= 14)
-            // Get the enum
-            return values()[value + 1];
-        else
-            // Throw illegal argument exception
-            throw new IllegalArgumentException(String.format("Controller value %d invalid", value));
-    }
-
-    /**
-     * Gets the value of the enum.
-     *
-     * @return The value.
-     */
-    public int getValue() {
-        return value;
-    }
+  /**
+   * Gets the value of the enum.
+   *
+   * @return The value.
+   */
+  public int getValue() {
+    return value;
+  }
 }
--- Java/src/com/copilot/yagl4j/test/Main.java
+++ Java/src/com/copilot/yagl4j/test/Main.java
@@ -7,53 +7,62 @@ import com.copilot.yagl4j.GamePadManager;
 
 public class Main implements GamePadListener {
 
-    static GamePadManager gamePadManager;
+  static GamePadManager gamePadManager;
 
-    public static void main(String[] args) {
-        new Main();
-        while (true) ;
-    }
+  public static void main(String[] args) {
+    new Main();
+    while (true);
+  }
 
-    public Main() {
-        gamePadManager = new GamePadManager();
-        gamePadManager.start();
-        gamePadManager.hookGamePadListener(this);
-        System.out.print("\033[2J\033[;H");
-    }
+  public Main() {
+    gamePadManager = new GamePadManager();
+    gamePadManager.start();
+    gamePadManager.hookGamePadListener(this);
+    System.out.print("\033[2J\033[;H");
+  }
 
-    @Override
-    public void onGamePadAdded(long gamePadID) {
-        System.out.println(String.format("Controller %d added", gamePadID));
-    }
+  @Override
+  public void onGamePadAdded(long gamePadID) {
+    System.out.println(String.format("Controller %d added", gamePadID));
+  }
 
-    @Override
-    public void onGamePadRemoved(long gamePadID) {
-        System.out.println(String.format("Controller %d removed", gamePadID));
-    }
+  @Override
+  public void onGamePadRemoved(long gamePadID) {
+    System.out.println(String.format("Controller %d removed", gamePadID));
+  }
 
-    @Override
-    public void onGamePadAxisMotion(long gamePadID, byte axis, short value) {
-        System.out.print(String.format("%c[%d;0f Controller %d moved axis %s: %d    ", 0x1B, axis + 1, gamePadID, GamePadAxis.fromValue(axis), value));
-        System.out.flush();
-    }
+  @Override
+  public void onGamePadAxisMotion(long gamePadID, byte axis, short value) {
+    System.out.print(String.format(
+        "%c[%d;0f Controller %d moved axis %s: %d    ", 0x1B, axis + 1,
+        gamePadID, GamePadAxis.fromValue(axis), value
+    ));
+    System.out.flush();
+  }
 
-    @Override
-    public void onGamePadButtonDown(long gamePadID, byte button) {
-        System.out.println(String.format("%c[%d;0f Controller %d %s: [x]", 0x1B, 7 + button, gamePadID, GamePadButton.fromValue(button)));
-    }
+  @Override
+  public void onGamePadButtonDown(long gamePadID, byte button) {
+    System.out.println(String.format(
+        "%c[%d;0f Controller %d %s: [x]", 0x1B, 7 + button, gamePadID,
+        GamePadButton.fromValue(button)
+    ));
+  }
 
-    @Override
-    public void onGamePadButtonUp(long gamePadID, byte button) {
-        System.out.println(String.format("%c[%d;0f Controller %d %s: [ ]", 0x1B, 7 + button, gamePadID, GamePadButton.fromValue(button)));
-    }
+  @Override
+  public void onGamePadButtonUp(long gamePadID, byte button) {
+    System.out.println(String.format(
+        "%c[%d;0f Controller %d %s: [ ]", 0x1B, 7 + button, gamePadID,
+        GamePadButton.fromValue(button)
+    ));
+  }
 
-    @Override
-    public void onGamePadRemapped(long gamePadID) {
-        System.out.println(String.format("Controller %d remapped", gamePadID));
-    }
+  @Override
+  public void onGamePadRemapped(long gamePadID) {
+    System.out.println(String.format("Controller %d remapped", gamePadID));
+  }
 
-    @Override
-    public void onGamePadError(String error) {
+  @Override
+  public void onGamePadError(String error) {
 
-    }
+  }
 }
